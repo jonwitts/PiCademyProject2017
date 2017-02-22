b97c2423aea1e908bf6079a37a406d8fc85edc3b
Jon Witts <jonwitts@users.noreply.github.com>
2017-02-22 20:46:41 +0000

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
-consumer_key = "**UPDATE**&"
-consumer_secret = "**UPDATE**"
-access_token = "**UPDATE**"
-access_token_secret = "**UPDATE**"
+# add your twitter app keys here...
+consumer_key = " **UPDATE** "
+consumer_secret = " **UPDATE** "
+access_token = " **UPDATE** "
+access_token_secret = " **UPDATE** "
